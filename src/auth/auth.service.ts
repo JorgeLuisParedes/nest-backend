ebe20658fefaf97f2e33e611dd4e0208b199400b
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-25 01:32:04 +0200

    Login - Verificar correo y contraseña

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {
 	BadRequestException,
 	Injectable,
 	InternalServerErrorException,
+	UnauthorizedException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -11,6 +12,7 @@ import * as bcryptjs from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -40,6 +42,23 @@ export class AuthService {
 				'Something terrible happend'
 			);
 		}
+	}
+
+	async login(loginDto: LoginDto) {
+		const { email, password } = loginDto;
+		const user = await this.userModel.findOne({ email });
+		if (!user)
+			throw new UnauthorizedException('Not valid credentials - Email');
+
+		if (!bcryptjs.compareSync(password, user.password))
+			throw new UnauthorizedException('Not valid credentials - Password');
+
+		const { password: _, ...rest } = user.toJSON();
+
+		return {
+			user: rest,
+			token: 'ABC-123',
+		};
 	}
 
 	findAll() {
